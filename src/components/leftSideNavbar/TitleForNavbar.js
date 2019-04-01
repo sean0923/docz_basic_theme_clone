e139e9a36e80df55848680b6c5e469d72117ec92
Sean Hong <iamseanhong@gmail.com>
2019-03-31 18:37:15 -0700

    Add rules to eslint

--- src/components/leftSideNavbar/TitleForNavbar.js
+++ src/components/leftSideNavbar/TitleForNavbar.js
@@ -6,12 +6,12 @@ import styled from 'styled-components';
 // -----------------------------------------------------------------------------------------
 const Wrapper = styled.div`
 	font-size: 24px;
-  font-weight: bold;
-  padding: 24px;
+	font-weight: bold;
+	padding: 24px;
 `;
 
 const Title = ({ text }) => {
-	return <Wrapper >{text}</Wrapper>;
+	return <Wrapper>{text}</Wrapper>;
 };
 
 export default Title;
